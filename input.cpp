ef7b0c54de250011583148c6affed2e3e003a6da
Fazzin <71731841+Fazzin@users.noreply.github.com>
2025-10-22 16:46:58 -0400

    Changes input.cpp

--- input.cpp
+++ input.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 
-class Keyboard {
-public:
-    std::string getInput() {
+class inputDevices {
+    public: 
+        virtual ~inputDevices() = default;
+        virtual std::string getInput() const = 0;
+};
+
+class Keyboard : public inputDevices {
+    std::string getInput() const override {
         return "User input from keyboard";
     }
 };
 
-class Mouse {
+class Mouse : public inputDevices {
 public:
-    std::string getInput() {
+    std::string getInput() const override {
         return "User input from mouse";
     }
 };
+
+// Future new input device
+// class Headphone : public inputDevices {}
 
 // Right now this code assumes that input will come from the keyboard, 
 // but we would like to be able to create an InputProcessor that 
@@ -21,15 +29,24 @@ public:
 // with one device.
 class InputProcessor {
 public:
-    void processInput() {
-        Keyboard keyboard;
-        std::string input = keyboard.getInput();
-        std::cout << "Processing: " << input;
+    explicit InputProcessor(const inputDevices& device) : device_(device) {}
+    void processInput() const {
+        std::string input = device_.getInput();
+        std::cout << "Processing: " << input << "\n";
     }
+
+private:
+    const inputDevices& device_;
 };
 
 // For demonstration
 int main() {
-    InputProcessor processor;
-    processor.processInput();
+
+    Keyboard keyboard;
+    Mouse mouse;
+    InputProcessor keyboardInput(keyboard);
+    InputProcessor mouseInput(mouse);
+
+    keyboardInput.processInput();
+    mouseInput.processInput();
 }
